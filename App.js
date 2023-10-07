e25f1b3b5753549733c6484e8a1893551278c20a
Samuel Abu <abu.sammie@gmail.com>
2023-10-07 22:00:15 +0200

    puzzle game started

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import CustomBtn from './components/CustomBtn';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CustomBtn />
+      <StatusBar style="dark" />
     </View>
   );
 }
